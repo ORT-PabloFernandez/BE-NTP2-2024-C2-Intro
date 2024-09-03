c04521ba39bca3b639b240ea804364f850709154
Pablo Fernandez Hinojosa <ppfernandez@hiberus.com>
2024-09-02 22:16:31 -0300

    + tarea de Beears

---
+++ ntp2-react-2024/src/mocBeers.js
@@ -0,0 +1,37 @@
+const beers = [
+  {
+    name: "Purple Iris",
+    abv: 6.8,
+    label:
+      "https://s3.amazonaws.com/brewerydbapi/beer/dMLwGo/upload_yiUllE-large.png",
+    type: "IPA",
+  },
+  {
+    name: "Orange Blossom Pilsner",
+    abv: 5.5,
+    label:
+      "https://s3.amazonaws.com/brewerydbapi/beer/Rczcb9/upload_9Nhxxl-large.png",
+    type: "Pilsner",
+  },
+  {
+    name: "Darkness",
+    abv: 4.2,
+    label:
+      "https://s3.amazonaws.com/brewerydbapi/beer/lnxbIV/upload_idNXFf-large.png",
+    type: "Stout",
+  },
+  {
+    name: "Belgian Wit",
+    abv: 5.4,
+    label:
+      "https://s3.amazonaws.com/brewerydbapi/beer/3CvVQG/upload_xOMnlK-large.png",
+    type: "Wheat",
+  },
+  {
+    name: "Stolen Fruit",
+    abv: 4.6,
+    label:
+      "https://s3.amazonaws.com/brewerydbapi/beer/YGT30k/upload_uVCHP7-large.png",
+    type: "Wheat",
+  },
+];
